b3f8d747c12ac3c8baf185800ff3793b8bfd3510
ahmadrzq <54834305+ahmadrzq@users.noreply.github.com>
2022-09-26 10:02:52 +0700

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+const {coffeeStock, isCoffeeMachineReady} = require('./state');
+
+const makeCoffee = (type,miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+        console.log("Kopi berhasil dibuat");
+    } else {
+        console.log("Biji kopi habis");
+    }
+}
+
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
+makeCoffee("robusta",80);
